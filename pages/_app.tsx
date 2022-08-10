7cc40dd2d4fd0f100760f562d131865259c10913
Mani Yeganeh <yeganehmani@gmail.com>
2022-08-10 12:49:42 +0430

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
+import Navbar from '../components/Navbar';
+import SideBar from '../components/SideBar';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+  if (isSSR) return null;
+  return (
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE}`}>
+      <Navbar />
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          <SideBar />
+        </div>
+        <div className="mt-4 flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+export default MyApp;
